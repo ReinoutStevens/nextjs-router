4ec4f5e744b1b91ba8297c1e3e1c3d86a376d1b8
Reinout Stevens <reinout@crazygames.com>
2022-09-19 14:24:36 +0200

    minimal case

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,27 @@
+import { useRouter } from "next/router"
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  const router = useRouter();
+
+  const brokenClick = () => {
+    router.push('http://localhost.com:3000/foo'); // note the .com part
+  }
+
+  const workingClick = () => {
+    router.push('http://localhost:3000/foo');
+  }
+
+  const workingExternalClick = ()=> {
+    router.push('http://www.google.com');
+  }
+
+  return (
+  <div>
+    <button onClick={workingClick}>Internal</button>
+    <button onClick={workingExternalClick}>External</button>
+    <button onClick={brokenClick}>Broken</button>
+    </div>
+    )
+    
 }
